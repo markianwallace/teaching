c9e4f546cf0d3b5bb29bf6012f3e398c709d9f8b
Mark Wallace <mark.wallace@kcl.ac.uk>
2017-10-05 14:22:40 +0100

    Let's upload the right folder this time

--- SingleMoleculeBiology/renderbook.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env Rscript
-if (file.exists("_main.Rmd")) file.remove("_main.Rmd")
-#if (file.exists("_book")) unlink('_book', recursive=TRUE)
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
-bookdown::render_book("index.Rmd", "bookdown::pdf_book")
