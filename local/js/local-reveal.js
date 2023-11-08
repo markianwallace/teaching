c636782f6bbf5971f3f9721b7119dca113a0e18a
Mark Wallace <markianwallace@gmail.com>
2023-11-08 10:24:56 +0000

    Presentation Skills Lecture

---
+++ local/js/local-reveal.js
@@ -0,0 +1,22 @@
+Reveal.initialize({
+  plugins: [RevealMath.KaTeX ],
+  // disableLayout: true,
+  //width: 1920,
+  //height: 1080,
+  width: 1024,
+  height: 768,
+  margin: 0.04,
+  controls: false,
+  progress: true,
+  autoPlayMedia: true,
+  viewDistance: 20,
+  mobileViewDistance: 5,
+  hash: true,
+  // Hides the address bar on mobile devices
+  hideAddressBar: true,
+  // Transition style
+  transition: "fade", // none/fade/slide/convex/concave/zoom
+  // Transition speed
+  transitionSpeed: "slow", // default/fast/slow,
+  preloadIframes: true
+});
